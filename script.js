029023ec468fa143d4dc134d021ed34d6c2b7fd6
Gabriela Trindade <byzinha88@hotmail.com>
2021-05-04 21:55:24 -0300

    Alteração de numero de celular

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const sendEmail = () => {
     let phone = document.getElementById("form_telefone");
     let name = document.getElementById("form_nomesobrenome");
     let email = document.getElementById("form_email");
-    window.open(`https://wa.me/5551981377877/?text=[${form_nomesobrenome.value}]%0D[${form_telefone.value}]%0D[${form_email.value}]%0D[${form_mensagem.value}]`);
+    window.open(`https://wa.me/5551997059342/?text=[${form_nomesobrenome.value}]%0D[${form_telefone.value}]%0D[${form_email.value}]%0D[${form_mensagem.value}]`);
 }
